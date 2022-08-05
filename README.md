cd2e3a7aaea15a6fb6de941fe29dde87c2d9ecd1
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-08-05 18:58:37 -0300

    criado pasta mocks

---
+++ README.md
@@ -0,0 +1,14 @@
+# Projeto CRUD
+
+## 🔖 Sobre
+
+Projeto crud seguindo padrao mrv, com as rotas GetALL, GetById, Put, Post e Delete
+
+CRUD com dados do anime cavaleiros do zodiaco
+
+Parte 1 utilizando dados mokados
+
+## ⚙ Tecnologias
+
+- JAVASCRIPT
+- EXPRESS
--- src/services/cavaleiro.service.js
+++ src/services/cavaleiro.service.js
@@ -1,37 +1,4 @@
-const cavaleiros = [
-  {
-    id: 1,
-    nome: 'Seiya',
-    constelacao: 'Pegasus',
-    categoria: 'Bronze',
-    tecnica: 'Meteoro de pegasus',
-    idade: 13,
-  },
-  {
-    id: 2,
-    nome: 'Shiryu',
-    constelacao: 'Dragão',
-    categoria: 'Bronze',
-    tecnica: 'Colera do Dragão',
-    idade: 14,
-  },
-  {
-    id: 3,
-    nome: 'Ikki',
-    constelacao: 'Fenix',
-    categoria: 'Bronze',
-    tecnica: 'Ave Fenix',
-    idade: 15,
-  },
-  {
-    id: 4,
-    nome: 'Mu',
-    constelacao: 'Aries',
-    categoria: 'Ouro',
-    tecnica: 'Extinção estelar',
-    idade: 20,
-  },
-];
+const cavaleiros = require('../mocks/cavaleiros');
 
 const findAllCavaleirosService = () => {
   return cavaleiros;
